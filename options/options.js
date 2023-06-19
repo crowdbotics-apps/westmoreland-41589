1e9e8eab099703ccee42d3b739e12fb5bab12ac4
Crowdbotics <team@crowdbotics.com>
2023-06-19 09:51:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "westmoreland-41589",
+  url: "https://westmoreland-41589.botics.co",
+  api: "https://westmoreland-41589.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
